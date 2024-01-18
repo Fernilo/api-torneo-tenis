4d55d4d2d462b7ca79e05e3998c2a8620a0b59e9
fernando@syloper.com <fernando@syloper.com>
2024-01-18 00:05:06 -0300

    Add jwt

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\MatchController;
 use App\Http\Controllers\Api\PlayerController;
 use App\Http\Controllers\Api\TournamentController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\WeatherController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,16 +19,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//sanctum no es necesario ya que contamos con jwt
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/players/pdf/{id}' , [PlayerController::class , 'pdf'])
+        ->name('players.pdf');
+    Route::post('tournaments/simulateTournament' , [TournamentController::class , 'simulateTournament'])
+        ->name('tournaments.simulate');
+
+    Route::apiResource('players',PlayerController::class);
+    Route::apiResource('tournaments',TournamentController::class);
+    Route::apiResource('matches',MatchController::class);
+    Route::get('/weather', [WeatherController::class, 'getWeather']);
+
+
 });
 
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+   
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('/me', [AuthController::class, 'me']);
+    Route::post('/register', [AuthController::class, 'register']);
+});
 
-Route::get('/players/pdf/{id}' , [PlayerController::class , 'pdf'])
-    ->name('players.pdf');
-Route::post('tournaments/simulateTournament' , [TournamentController::class , 'simulateTournament'])
-    ->name('tournaments.simulate');
-Route::apiResource('players',PlayerController::class);
-Route::apiResource('tournaments',TournamentController::class);
-Route::apiResource('matches',MatchController::class);
-Route::get('/weather', [WeatherController::class, 'getWeather']);
